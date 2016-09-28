56aa40dfd997e9f7c8d38ad88b23678b4578a5e4
Sven Fischer <git-dev@linux4tw.de>
2016-09-28 17:02:50 +0200

    implement getBodyOfAPIURL with added header for auth

--- fetch.go
+++ fetch.go
@@ -1,4 +1,4 @@
-package main
+package wallabago
 
 import (
 	"fmt"
@@ -7,35 +7,43 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"time"
 )
-
-func main() {
-	start := time.Now()
-	log.SetOutput(os.Stdout)
-	log.Print("fetch")
-	loginPage := getBodyOfURL("https://wallabag.linux4tw.de")
-	fmt.Printf("%s\n", loginPage)
-	log.Printf("csrfToken: %v\n", getCSRFToken(loginPage))
-	log.Printf("time elapsed: %.2fs\n", time.Since(start).Seconds())
-}
 
 // makes a HTTP request and returns the HTML code of that URL
 func getBodyOfURL(url string) string {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		fmt.Fprintf(os.Stderr, "getBodyOfURL: %v\n", err)
 		os.Exit(1)
 	}
+	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		fmt.Fprintf(os.Stderr, "getBodyOfURL: reading %s: %v\n", url, err)
 		os.Exit(1)
 	}
 	log.Print(resp.Status)
 	//fmt.Printf("%s", b)
 	return string(b)
+}
+
+func getBodyOfAPIURL(url string) []byte {
+	if token.TokenType == "" || token.AccessToken == "" {
+		token = GetToken()
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	authString := strings.ToUpper(string(token.TokenType[0])) + token.TokenType[1:len(token.TokenType)] + " " + token.AccessToken
+	// log.Print("getBodyOfAPIURL: authString=" + authString)
+	req.Header.Add("Authorization", authString)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "getBodyOfAPIURL: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	return body
 }
 
 // parses the HTML code from the wallabag v2 login page to retrieve the CSRF token and returns that
