e997922cfdd08f82909b9d3f1918ea5aae899aff
Sven Fischer <git-dev@linux4tw.de>
2016-10-05 21:12:30 +0200

    version API call returns string in quotation marks, which are stripped off now

--- version.go
+++ version.go
@@ -3,5 +3,6 @@ package wallabago
 // Version returns the version of the configured wallabag instance
 func Version() string {
 	v := getBodyOfURL(Config.WallabagURL + "/api/version")
-	return v[1 : len(v)-2]
+	// strip of the quotation marks from the version string being return from the API
+	return v[1 : len(v)-1]
 }
