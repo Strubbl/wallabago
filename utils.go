ac3a3c36c7ae9b72356645336cff05f512dabc08
Sven Fischer <git-dev@linux4tw.de>
2016-11-21 19:20:54 +0100

    remove not used func getCSRFToken

--- utils.go
+++ utils.go
@@ -48,19 +48,3 @@ func getBodyOfAPIURL(url string) []byte {
 	}
 	return body
 }
-
-// parses the HTML code from the wallabag v2 login page to retrieve the CSRF token and returns that
-func getCSRFToken(htmlCode string) string {
-	const csrfTokenStartRegex string = "<input type=\"hidden\" name=\"_csrf_token\" value=\""
-	csrfTokenStart := len(csrfTokenStartRegex) + strings.Index(htmlCode, csrfTokenStartRegex)
-	log.Printf("csrfTokenStart: %d\n", csrfTokenStart)
-	if csrfTokenStart < 0 {
-		log.Print("csrf token not found in login page")
-		os.Exit(1)
-	}
-	csrfTokenEnd := csrfTokenStart + strings.Index(htmlCode[csrfTokenStart:], "\" />")
-	log.Printf("csrfTokenEnd: %d\n", csrfTokenEnd)
-	csrfToken := htmlCode[csrfTokenStart:csrfTokenEnd]
-	log.Printf("csrfToken: %v\n", csrfToken)
-	return csrfToken
-}
