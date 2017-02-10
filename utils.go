50b2339ca182f263b2b5fcab953c0a5967c47dd2
Sven Fischer <git-dev@linux4tw.de>
2017-02-10 22:45:27 +0100

    add util function for POST request to API

--- utils.go
+++ utils.go
@@ -45,3 +45,28 @@ func getBodyOfAPIURL(url string) []byte {
 	}
 	return body
 }
+
+func postToAPI(apiURL string, postData []byte) {
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", apiURL, strings.NewReader(string(postData)))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "postToAPI: posting form %v\n", err)
+	}
+
+	// auth
+	if token.TokenType == "" || token.AccessToken == "" {
+		token = getToken()
+	}
+	authString := strings.ToUpper(string(token.TokenType[0])) + token.TokenType[1:len(token.TokenType)] + " " + token.AccessToken
+	req.Header.Add("Authorization", authString)
+	// exec POST request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "postToAPI: error while getting response of our POST request %v\n", err)
+	}
+	defer resp.Body.Close()
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "postToAPI: error while ioutil.ReadAll %v\n", err)
+	}
+}
